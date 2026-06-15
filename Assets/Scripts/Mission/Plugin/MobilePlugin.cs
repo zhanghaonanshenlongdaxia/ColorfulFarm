30e22ce1f27aab8b680ef16ca3950c061afcc2d3
zhnghaonan <798043862@qq.com>
2026-06-15 16:06:46 +0800

    chore: remove legacy Android sdk payloads for test build

--- Assets/Scripts/Mission/Plugin/MobilePlugin.cs
+++ Assets/Scripts/Mission/Plugin/MobilePlugin.cs
@@ -4,10 +4,6 @@ using System.Collections;
 public class MobilePlugin
 {
     private static MobilePlugin instance;
-#if UNITY_ANDROID
-    private AndroidJavaClass objJavaClass;
-    private AndroidJavaObject objJavaObject;
-#endif
 
     public static MobilePlugin getInstance()
     {
@@ -20,29 +16,15 @@ public class MobilePlugin
 
     private MobilePlugin()
     {
-#if UNITY_ANDROID
-        objJavaClass = new AndroidJavaClass("com.example.androidunityplugin.AndroidUnity");
-        objJavaObject = new AndroidJavaObject("com.example.androidunityplugin.AndroidUnity");
     }
-#else
-    }
-#endif
 
     public void ShowToast(string message, bool length_show = false)
     {
-#if UNITY_ANDROID
-        objJavaClass.CallStatic("Toast", message, length_show);
-#else
         Debug.Log(message);
-#endif
 	}
 
     public void ShowExitConfirm(string title, string message, string ok, string cancel)
     {
-#if UNITY_ANDROID
-        objJavaClass.CallStatic("ShowCofirm", title, message, ok, cancel);
-#else
         Debug.Log(title + ": " + message);
-#endif
     }
 }
